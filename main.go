584bd6b97aea76d641c4d97e962817a4a4a1665f
Emily Reese <eareese@eareese.com>
2017-10-26 10:43:58 -0400

    Implement decode for some stuff and refactor

--- main.go
+++ main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"math"
 	"os"
-	"path"
+	"strings"
 )
 
 // Pattern has the details of decoded drum machine patterns.
 type Pattern struct {
 	version string
+	tempo   float32
 }
 
 func (p *Pattern) String() string {
-	return fmt.Sprintf("Saved with HW Version: %s\n", p.version)
+	return fmt.Sprintf(`Saved with HW Version: %s
+Tempo: %v
+`, p.version, p.tempo)
 }
 
 func main() {
-	hexdump(path.Join("fixtures", "pattern_5.splice"))
 }
 
 // hexdump scans the given file and dumps the contents to Stdout.
 func hexdump(fpath string) {
 	f, err := os.Open(fpath)
 	defer f.Close()
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	stdoutDumper := hex.Dumper(os.Stdout)
 	defer stdoutDumper.Close()
@@ -39,6 +42,27 @@ func hexdump(fpath string) {
 }
 
 // DecodeFile decodes the drum machine files.
-func DecodeFile(path string) (*Pattern, error) {
-	return &Pattern{}, nil
+func DecodeFile(fpath string) (*Pattern, error) {
+	p := Pattern{}
+	f, err := os.Open(fpath)
+	defer f.Close()
+	check(err)
+
+	contents, err := ioutil.ReadFile(fpath)
+	check(err)
+
+	// spliceInfo := string(contents[:6])
+	version := strings.Trim(string(contents[14:27]), "\x00")
+	p.version = version
+
+	tempo := math.Float32frombits(binary.LittleEndian.Uint32(contents[46:50]))
+	p.tempo = tempo
+
+	return &p, nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
